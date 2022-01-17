c0754d4cb3667e749f170e38bc641278ba7fc921
Mateusz Sobiech <lordpython8@gmail.com>
2022-01-17 15:04:55 +0100

    add scrollTop()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    let language = localStorage.getItem('lan');
-    if (language === null) {
-      localStorage.setItem('lan', 'pl');
+  ngOnInit(): void {}
+
+  scrollTop() {
+    let top = document.getElementById('top');
+    if (top !== null) {
+      top.scrollIntoView();
+      top = null;
     }
   }
 }
